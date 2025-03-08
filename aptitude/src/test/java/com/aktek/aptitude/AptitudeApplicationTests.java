b1897f1358ff3d1ee6dad918a1bc7d881181224a
sathish-s-dev <sathishs.webdev@gmail.com>
2025-03-08 09:35:58 +0530

    feat: project created and dependancies added

---
+++ aptitude/src/test/java/com/aktek/aptitude/AptitudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktek.aptitude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptitudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
